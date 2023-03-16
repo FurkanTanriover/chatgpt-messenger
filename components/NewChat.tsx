1a279842c033762930b06b33070a9cb9daf4e5b5
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-03-16 16:10:28 +0300

    implement create new chat

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import { PlusIcon } from '@heroicons/react/24/outline'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
+import { db } from '../firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { timeStamp } from 'console';
 
 const NewChat = () => {
+  const {data: session} = useSession();
+  const router = useRouter();
+  
+  const createNewChat = async() => {
+      const doc = await addDoc(collection(db, "users",session?.user?.email!,"chats"), {
+        userId:session?.user?.email!,
+        createdAt:serverTimestamp()
+      });
+      router.push(`/chat/${doc.id}`)
+  }
+
   return (
-    <div className=' border-gray-700 border chatRow'>
+    <div onClick={createNewChat} className=' border-gray-700 border chatRow'>
         <PlusIcon className='w-4 h-4 iconColor'/>
         <p className=''>New Chat</p>
     </div>
