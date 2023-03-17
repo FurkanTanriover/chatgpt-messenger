f14d8d0ec0bcc4425584b59b0f4370d2e5ecf661
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-03-17 13:40:56 +0300

    implementing messaging functionality

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import { useSession } from "next-auth/react";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import {toast} from "react-hot-toast";
 
 type Props = {
   chatId: string;
@@ -9,10 +12,48 @@ type Props = {
 
 function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState<string>("");
-  const {data:session} = useSession();
+  const { data: session } = useSession();
+
+  // TODO: useSWR to get model
+
+  const model = "text-davinci-003";
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+    const input = prompt.trim();
+    setPrompt("");
+    const message: Message = {
+      text: input,
+      createdAt: new Date().toISOString(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+    await addDoc(collection(db, "users", session?.user?.email!, "chats", chatId, "messages"), message);
+    //toast notification to say loading!
+    const notification =toast.loading("ChatGPT is thinking...")
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(()=>{
+        // toast notification succesfully
+        toast.success("ChatGPT has responded!",{id:notification})
+    })
+  };
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
-      <form className="p-5 space-x-5 flex">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
         <input
           type="text"
           className="bg-transparent focus:outline-none disabled:cursor-not-allowed disabled:text-gray-300 flex-1"
@@ -21,7 +62,11 @@ function ChatInput({ chatId }: Props) {
           placeholder="Enter your message..."
           onChange={(e) => setPrompt(e.target.value)}
         />
-        <button disabled={!prompt||!session} type="submit" className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed">
+        <button
+          disabled={!prompt || !session}
+          type="submit"
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+        >
           <PaperAirplaneIcon className="h-4 w-4  -rotate-45" />
         </button>
       </form>
