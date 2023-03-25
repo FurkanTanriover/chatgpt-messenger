0f25902296581c674f55aa4546c872391b53f6ab
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-03-25 14:47:00 +0300

    added model selection feature

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -5,6 +5,8 @@ import React, { FormEvent, useState } from "react";
 import { addDoc, collection, serverTimestamp, Timestamp } from "firebase/firestore";
 import { db } from "../firebase";
 import {toast} from "react-hot-toast";
+import ModelSelection from "./ModelSelection";
+import  useSWR  from "swr";
 
 type Props = {
   chatId: string;
@@ -13,10 +15,11 @@ type Props = {
 function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState<string>("");
   const { data: session } = useSession();
+  const { data: model } = useSWR("model", { fallbackData: "text-davinci-003" });
+
 
   // TODO: useSWR to get model
 
-  const model = "text-davinci-003";
 
   const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -70,7 +73,9 @@ function ChatInput({ chatId }: Props) {
           <PaperAirplaneIcon className="h-4 w-4  -rotate-45" />
         </button>
       </form>
-      <div></div>
+        <div className="md:hidden">
+            <ModelSelection/>
+        </div>
     </div>
   );
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,35 +1,44 @@
-"use client"
-import {collection} from "firebase/firestore";
+"use client";
+import { collection } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react";
 import React from "react";
 import NewChat from "./NewChat";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
 import ChatRow from "./ChatRow";
-
+import ModelSelection from "./ModelSelection";
 
 function SideBar() {
   const { data: session } = useSession();
-  const [chats, loading, error] = useCollection(
-    session && collection(db, "users", session.user?.email!, "chats")
-  )
-  console.log(chats?.docs)
+  const [chats, loading, error] = useCollection(session && collection(db, "users", session.user?.email!, "chats"));
+  console.log(chats?.docs);
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
           {/* new chat */}
           <NewChat />
-          <div>{/* model selection */}</div>
-          {/* chat history */}
-          {chats?.docs.map((chat) => (
-            <ChatRow key={chat.id} id={chat.id}  />
+          <div className="hidden sm:inline">
+            {" "}
+            <ModelSelection />
+          </div>
+          <div className="flex flex-col space-y-2 my-2">
+          {loading && <div className="animate-pulse text-center text-white"><p>Loading Chats...</p></div>}
+          {
+          chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
           ))}
+          </div>
         </div>
       </div>
-      {session &&
-      ( <img onClick={()=>signOut()} src={session.user?.image!} alt="user pic" className="w-12 h-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" />)
-      }
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="user pic"
+          className="w-12 h-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 }
